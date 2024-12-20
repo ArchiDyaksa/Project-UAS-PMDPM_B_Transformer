47eb5d3edb99a4edf430930d711ea2f1fb8cd028
Archipera Rari Dyaksa <220711891@students.uajy.ac.id>
2024-12-20 12:18:26 +0700

    Update MainStreamlit_B_Transformer.py

--- MainStreamlit_B_Transformer.py
+++ MainStreamlit_B_Transformer.py
@@ -56,12 +56,12 @@ def get_base64(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-def set_background(jpg_file):
-    bin_str = get_base64(jpg_file)
+def set_background(png_file):
+    bin_str = get_base64(png_file)
     page_bg_img = '''
     <style>
     .stApp {
-    background-image: url("data:image/jpg;base64,%s");
+    background-image: url("data:image/png;base64,%s");
     background-size: cover;
     }
     </style>
